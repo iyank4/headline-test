537ea086bf0762b44b0f2207a1be517c97cefa45
Iyan Kushardiansah <hello@iyank4.com>
2016-05-14 07:04:27 +0700

    placeholder

---
+++ copy_assets.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp -rf bower_components/topcoat/css .
+cp -rf bower_components/topcoat/img .
+cp -rf bower_components/topcoat/font .
+cp -f bower_components/sizzle/dist/sizzle.min.js js/
+cp -f bower_components/sizzle/dist/sizzle.min.map js/
+
